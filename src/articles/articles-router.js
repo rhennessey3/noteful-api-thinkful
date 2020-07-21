ab76adce70e482ad8581a456b742cbdd631e0a9d
rick <rhennessey3@gmail.com>
2020-07-21 17:57:34 -0400

    almost to react

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -79,7 +79,7 @@ articlesRouter
             date_published: res.article.date_published,
         })
         const knexInstance = req.app.get('db')
-        
+
     })
     .delete((req, res, next) => {
         ArticlesService.deleteArticle(
@@ -87,6 +87,30 @@ articlesRouter
             req.params.article_id
         )
             .then(() => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+    .patch(jsonParser, (req, res, next) => {
+        const { title, content, style } = req.body
+        const articleToUpdate = { title, content, style }
+
+        const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length
+        if (numberOfValues === 0) {
+            return res.status(400).json({
+                error: {
+                    message: `Request body must contain either 'title', 'style' or 'content'`
+                }
+            })
+        }
+
+        ArticlesService.updateArticle(
+            req.app.get('db'),
+            req.params.article_id,
+            articleToUpdate
+        )
+            .then(numRowsAffected => {
                 res.status(204).end()
             })
             .catch(next)
