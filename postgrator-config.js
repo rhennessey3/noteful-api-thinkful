b23c4d6882909a2bc46a5108a3487a7fbf5d7774
rick <rhennessey3@gmail.com>
2020-08-07 17:26:44 -0400

    added pg cli

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
+    "ssl": process.env.SSL,
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DB_URL
         : process.env.DB_URL,
